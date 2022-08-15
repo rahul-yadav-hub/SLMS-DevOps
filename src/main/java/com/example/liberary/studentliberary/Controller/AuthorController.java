8cb01a31daa8abf65cc0be45d333e8a76e75b4c8
Rahul Yadav <55504874+rahul-yadav-hub@users.noreply.github.com>
2022-08-16 00:36:36 +0530

    Update AuthorController.java

--- src/main/java/com/example/liberary/studentliberary/Controller/AuthorController.java
+++ src/main/java/com/example/liberary/studentliberary/Controller/AuthorController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AuthorController
