832859b14917c95bd31762bd8f7b6ef9dbf9cbb2
Rahul Yadav <55504874+rahul-yadav-hub@users.noreply.github.com>
2022-08-16 10:41:24 +0530

    Update Author.java

--- src/main/java/com/example/liberary/studentliberary/Model/Author.java
+++ src/main/java/com/example/liberary/studentliberary/Model/Author.java
@@ -84,7 +84,7 @@ public class Author
     }
     public String toString() {
         return "Author{" +
-                 ", id='" + id + '\'' 
+                 ", id='" + id + '\'' +
                 ", name='" + name + '\'' +
                 ", email='" + email + '\'' +
                 ", age=" + age +
